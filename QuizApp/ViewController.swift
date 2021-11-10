99ca74fb097c543b9cf06d7fc7c3aa00b4d948cd
yashkumar patel <ysp18@scarletmail.rutgers.edu>
2021-11-10 16:19:27 -0500

    Quiz app

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -8,12 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var questions: UILabel!
+    
+    var questionBank = [["2 + 2 = 4","True"],
+                        ["6 - 2 = 5","False"],
+                        ["6 + 2 = 9","False"]]
+    var questionNumber = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateUI()   //calling a function
+        
     }
 
-
+    @objc func updateUI(){
+        
+        questions.text = questionBank[questionNumber][0]
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        
+        let userInput = sender.currentTitle!
+        
+        if userInput == questionBank[questionNumber][1] {
+            
+        sender.backgroundColor = UIColor.green
+            
+        }
+        else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        if questionNumber + 1 < questionBank.count {
+            
+            questionNumber += 1
+        }
+        
+        
+        else {
+            
+            questionNumber = 0
+        }
+     
+        Timer.scheduledTimer(timeInterval: 0.4, target: self, selector: #selector(updateUI), userInfo: nil, repeats: true)
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
 }
 
